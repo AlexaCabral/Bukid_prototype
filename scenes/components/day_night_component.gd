86001ba63e2bc3ab37244b908c4aaaf9bef2e7b0
aelancupido <amcupido@up.edu.ph>
2025-05-20 23:11:33 +0800

    fixed day night gradient and added day night ui

--- scenes/components/day_night_component.gd
+++ scenes/components/day_night_component.gd
@@ -1,32 +1,9 @@
 class_name DayNightComponent
 extends CanvasModulate
 
-@export var initial_day: int = 1:
-	set(id):
-		initial_day = id
-		DayNightManager.initial_day = id
-		DayNightManager.set_initial_time()
-
-@export var initial_hour: int = 12:
-	set(ih):
-		initial_hour = ih
-		DayNightManager.initial_hour = ih
-		DayNightManager.set_initial_time()
-
-@export var initial_minute: int = 30:
-	set(im):
-		initial_minute = im
-		DayNightManager.initial_minute = im
-		DayNightManager.set_initial_time()
-		
 @export var day_night_gradient_texture: GradientTexture1D
 
 func _ready() -> void:
-	DayNightManager.initial_day = initial_day
-	DayNightManager.initial_hour = initial_hour
-	DayNightManager.initial_minute = initial_minute
-	DayNightManager.set_initial_time()
-	
 	DayNightManager.game_time.connect(on_game_time)
 	
 func on_game_time(time: float) -> void:
